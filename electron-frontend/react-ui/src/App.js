9198c62dcb69b8fcf8c196f19f67189f4c9c8e3f
Daniel <dxs.accouts@web.de>
2024-12-23 16:39:27 +0100

    added routing to basic pages

--- electron-frontend/react-ui/src/App.js
+++ electron-frontend/react-ui/src/App.js
@@ -1,13 +1,20 @@
 import HomeScreen from "./screens/home";
 import AddGuitarScreen from "./screens/addGuitar";
-import { BrowserRouter, Route, Routes } from "react-router";
+import AddMaintenanceScreen from "./screens/addMaintenance";
+import ViewGuitarsScreen from "./screens/viewGuitars";
+import ViewMaintenancesScreen from "./screens/viewMaintenances";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeScreen/>}/>
-        <Route path="/add-guitar" element={<AddGuitarScreen/>}/>
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/add-guitar" element={<AddGuitarScreen />} />
+        <Route path="/add-maintenance" element={<AddMaintenanceScreen />} />
+        <Route path="/view-guitars" element={<ViewGuitarsScreen />} />
+        <Route path="/view-maintenances" element={<ViewMaintenancesScreen />} />
+        <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
     </BrowserRouter>
   )
